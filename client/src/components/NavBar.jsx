6a4c53abd17284632500f3eda09cf6b7a9f0189c
Thinh Nguyen <swe.thinhnguyen@gmail.com>
2024-11-17 22:46:18 -0600

    build nav bar on mobile screen

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import { Link, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse, faUser, faFile, faPhone, faDiagramProject } from "@fortawesome/free-solid-svg-icons";
+
+function NavBar() {
+    const [isOpen, setIsOpen] = useState(false);
+    const currentPage = useLocation().pathname;
+    const MENU = [
+        { name: "Home", link: "/", icon: faHouse },
+        { name: "About me", link: "/about", icon: faUser },
+        { name: "Resume", link: "/resume", icon: faFile },
+        { name: "Works", link: "/works", icon: faDiagramProject },
+        { name: "Contact", link: "/contact", icon: faPhone }
+    ];
+    const menuHandler = (e) => {
+        if(e.target.closest('.menu-item') || e.target.closest(".menu-toggle")) {
+            setIsOpen(!isOpen);
+        }
+    }
+
+    return (
+        <nav>
+            <div className="nav-container">
+                <div className="nav-left">
+                    <Link to="/" className="link home-link" onClick={() => setIsOpen(false)}>
+                        <span>Charles</span>
+                    </Link>
+                </div>
+                <div className={isOpen ? "nav-right open" : "nav-right"} onClick={(e) => menuHandler(e)}>
+                    <div className='menu-toggle'>
+                        <span className="bar"></span>
+                    </div>
+                    <ul className="list menu">
+                        {MENU.map((item, index) => (
+                            <li className="menu-item" key={index}>
+                                <Link to={item.link} className="link">
+                                    <FontAwesomeIcon icon={item.icon} className="icon" />
+                                    <span>{item.name}</span>
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,41 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from './pages/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      // {
+      //   path: '/about',
+      //   element: <About />,
+      // },
+      // {
+      //   path: '/resume',
+      //   element: <Resume />,
+      // },
+      // {
+      //   path: '/works',
+      //   element: <Works />,
+      // },
+      // {
+      //   path: '/contact',
+      //   element: <Contact />,
+      // },
+    ],
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
