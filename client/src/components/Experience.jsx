039a3262a60a7f93ff46a503b395dba61c25f4db
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-11-22 23:43:16 -0600

    build experience compoent on mobile and bigger screens

--- client/src/components/Experience.jsx
+++ client/src/components/Experience.jsx
@@ -1,9 +1,41 @@
 function Experience () {
+    const EXPERIENCES = [
+        {
+            position: "Backend Developer Internship",
+            company: "Chosn App",
+            time: "May 2023 - Aug 2023",
+            description: [
+                "Collaborated with a team of three engineers to build robust backend services using AWS technologies and Node.js,enhancing the reliability and scalability of the application.",
+                "Contributed to the creation of a reminder service that helped users manage and complete daily tasks, resulting in increased user engagement and improved mental health support.",
+                "Refactored and optimized existing code to boost readability and database performance, leading to more efficient operations and reduced maintenance time."
+            ]
+        }
+    ]
+
     return (
-        <div className="experience">
-            <h2>Experience</h2>
-            <p>Experience</p>
-        </div>
+        <>
+            <h2>EXPERIENCE</h2>
+            {EXPERIENCES.map((experience, index) => {
+                return (
+                    <div key={index} className="experience">
+                        <h3>{experience.position}</h3>
+                        <div className="container">
+                            <p className="experience-company">{experience.company}</p>
+                            <p>{experience.time}</p>
+                        </div>
+                        <ul className="experience-description">
+                            {experience.description.map((description, index) => {
+                                return (
+                                    <li key={index}>
+                                        <p>{description}</p>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                )
+            })}
+        </>
     )
 }
 
