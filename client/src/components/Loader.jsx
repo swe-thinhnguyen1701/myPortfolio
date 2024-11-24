514ebe2b6893fc926a83e930f5f0ec3bf210c29c
Thinh Nguyen <swe.thinhnguyen@gmail.com>
2024-11-23 23:11:42 -0600

    build loader component

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,13 @@
+function Loader() {
+    return (
+        <div className="loader-container">
+            <svg viewBox="0 0 400 160" className="loader">
+                <text x="50%" y="70%" dy=".32rem" textAnchor="middle" className="text-body">
+                    Charles
+                </text>
+            </svg>
+        </div>
+    )
+}
+
+export default Loader;
