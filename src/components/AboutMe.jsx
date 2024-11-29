6f108131afd20055466dbd7dfe926391710f67ae
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-11-29 10:49:40 -0600

    update about me section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,17 +1,20 @@
+import Name from "./Name";
+
 function AboutMe() {
     return (
         <>
             <h2>About Me</h2>
-            <p>
-                Hi, I’m Charles, a passionate and driven full-stack developer with a strong foundation in both front-end and back-end development. I graduated from the Full-Stack Development Bootcamp at UC Berkeley, where I honed my skills in technologies like React, Node.js, MongoDB, PostgreSQL, and more.
+            <Name />
+            <p className="about-me">
+                I'm a passionate and driven full-stack developer with a strong foundation in both front-end and back-end development. I graduated from the Full-Stack Development Bootcamp at UC Berkeley, where I honed my skills in technologies like React, Node.js, MongoDB, PostgreSQL, and more.
             </p>
-            <p>
+            <p className="about-me">
                 My journey into coding began with a fascination for creating innovative solutions that make a real impact. I thrive on challenges, enjoy solving complex problems, and take pride in building user-friendly, efficient applications.
             </p>
-            <p>
+            <p className="about-me">
                 Currently, I’m focused on landing my first role in the tech industry, where I can apply my technical expertise and dedication to contribute to impactful projects. Alongside my professional growth, I’m eager to connect with like-minded individuals who share a passion for technology and innovation.
             </p>
-            <p>
+            <p className="about-me">
                 Let’s connect and build something great together!
             </p>
         </>
